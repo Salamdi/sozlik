a3124327af6b5db461503b808d1784c32540ddb4
Dinislam Abdulsalamov <dinislam@MBP-Dinislam-2.moscow.epam.com>
2018-04-18 18:09:24 +0300

    add snackbar

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Drawer from 'material-ui/Drawer'
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
 import NavigationClose from 'material-ui/svg-icons/navigation/close'
 import IconButton from 'material-ui/IconButton'
+import Snackbar from 'material-ui/Snackbar'
 
 class App extends Component {
   constructor(props) {
@@ -64,9 +65,12 @@ class App extends Component {
         .then(result => result.data)
         .then(response => this.setState({ results: response.data.length ? response.data : [{ru: '', ng: 'Бу соьз соьзликте йок ):'}], circularProgress: false }))
         .catch(err => {
-          console.error(err)
+          console.error('error occured:', err)
           this.setState({ circularProgress: false })
-          console.log('Серверга йолыкпага болынмады...')
+          if (err.message === 'Failed to fetch' || err.message === 'Network Error') {
+            console.log(err.message)
+            this.setState({snackbar: true})
+          }
         })
     }
   }
@@ -87,39 +91,47 @@ class App extends Component {
   render() {
     return (
       <div className={this.state.focus ? 'App offset' : 'App'} >
-      <Drawer
-        docked={false}
-        width={200}
-        open={this.state.drawer}
-        onRequestChange={(open) => this.setState({drawer: open})}
-      >
-        <AppBar
-          showMenuIconButton={false}
-          iconElementRight={
-            <IconButton onClick={() => this.setState({drawer: false})} >
-              <NavigationClose />
-            </IconButton>
-          }
-        />
-        <RadioButtonGroup
-          name='lang'
-          defaultSelected='ru-ng'
-          className='radio-container'
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.drawer}
+          onRequestChange={(open) => this.setState({drawer: open})}
         >
-          <RadioButton
-            value='ru-ng'
-            label='орысша - ногайша'
+          <AppBar
+            showMenuIconButton={false}
+            iconElementRight={
+              <IconButton onClick={() => this.setState({drawer: false})} >
+                <NavigationClose />
+              </IconButton>
+            }
           />
-          <RadioButton
-            value='ng-ru'
-            label='ногайша - орысша'
-            disabled
-          />
-        </RadioButtonGroup>
-        <p className='copyright'>
-          © Dinislam
-        </p>
-      </Drawer>
+          <RadioButtonGroup
+            name='lang'
+            defaultSelected='ru-ng'
+            className='radio-container'
+          >
+            <RadioButton
+              value='ru-ng'
+              label='орысша - ногайша'
+            />
+            <RadioButton
+              value='ng-ru'
+              label='ногайша - орысша'
+              disabled
+            />
+          </RadioButtonGroup>
+          <p className='copyright'>
+            © Dinislam
+          </p>
+        </Drawer>
+        <Snackbar
+          open={this.state.snackbar || false}
+          message='Интернет йок усайды...'
+          action='аьруьв'
+          autoHideDuration={4000}
+          onActionClick={() => this.setState({snackbar: false})}
+          onRequestClose={() => this.setState({snackbar: false})}
+        />
         <div className='top-container'>
           <AppBar
             title='Соьзлик'
